db5faa7ac26c401daa0a47c4c9287ec4d9037f70
Henry Paulino <henrypl360@gmail.com>
2021-01-21 18:57:28 -0500

    Initial Commit

---
+++ eCommerce Example/eCommerce_ExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  eCommerce_ExampleApp.swift
+//  eCommerce Example
+//
+//  Created by Henry Paulino on 1/21/21.
+//
+
+import SwiftUI
+
+@main
+struct eCommerce_ExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
